742c897f785a0a5f7f5d0f862342fe0fa6f2ce15
carlosyanez <carlos.yanez.s@gmail.com>
2021-04-06 14:19:44 +0200

    codified theme, image crop and save into customthemes package

--- bushfires_au2020_eu/bushfires_au2020_eu.R
+++ bushfires_au2020_eu/bushfires_au2020_eu.R
@@ -10,7 +10,7 @@ librarian::shelf("tidyverse","sf","giscoR","units","measurements",
 
 area_burnt_aus <- as_units(10173*10^3*10^4,"m^2") # #from https://www.agriculture.gov.au/abares/forestsaustralia/forest-data-maps-and-tools/fire-data#area-of-native-forest-in-fire-area-by-forest-tenure-and-jurisdiction
 area_burnt_us <-  as_units(5299044.8*10^4,"m^2") # https://en.wikipedia.org/wiki/2020_wildfire_season
-area_burnt_bra <- as_units((376416)*10^4,"m^2") # https://en.wikipedia.org/wiki/2020_C%C3%B3rdoba_wildfires https://en.wikipedia.org/wiki/2020_Delta_del_Paran%C3%A1_wildfires 
+area_burnt_bra <- as_units((376416)*10^4,"m^2") # https://en.wikipedia.org/wiki/2020_C%C3%B3rdoba_wildfires https://en.wikipedia.org/wiki/2020_Delta_del_Paran%C3%A1_wildfires
 
 ### two functions to get equivalent surfaces
 get_contiguous <- function(other,base){
@@ -21,53 +21,53 @@ get_contiguous <- function(other,base){
   return(contiguous)
 }
 map_fires <- function(eu_nuts,starting_id,area_burnt){
-  
+
   base <- eu_nuts %>%filter(NUTS_ID==starting_id)
   continue <- TRUE
-  
+
   #add all contiguous areas until exceeding 1.1 of aussie area
-  
+
   while(continue){
-    
+
     cont1 <- get_contiguous(eu_nuts %>% filter(!(NUTS_ID %in% base$NUTS_ID)),
                             base)
-    
+
     total_area <- sum(st_area(base)) + sum(st_area(cont1))
     continue <- area_burnt > total_area
-    
+
     if(continue) base <- rbind(base,cont1)
   }
-  
+
   remnant <- area_burnt - sum(st_area(base))
-  
+
   cont1$area <- st_area(cont1)
-  
+
   cont1 <- cont1 %>% mutate(area_rel = area/remnant) %>%
     arrange((area_rel)) %>%
     mutate(cumul_area =cumsum(area_rel))
-  
-  cont1$cumul_area <- drop_units(cont1$cumul_area)  
-  
-  base <- rbind(base, 
+
+  cont1$cumul_area <- drop_units(cont1$cumul_area)
+
+  base <- rbind(base,
                 cont1 %>% filter(cumul_area <= 1) %>%
                   select(-cumul_area,-area_rel,-area))
-  
+
   remnant <- area_burnt - sum(st_area(base))
-  
-  
+
+
   cont1 <- cont1  %>% filter(!(NUTS_ID %in% base$NUTS_ID)) %>%
                       slice_head(n=1)
-                              
+
   if(drop_units(cont1[1,]$area/remnant)>0.5){
-    base <- rbind(base, 
+    base <- rbind(base,
                   cont1  %>%
                     select(-cumul_area,-area_rel,-area))
-    
+
   }
-  
+
 
   return(base)
-  
+
 }
 
 #get eurostat areas
@@ -90,7 +90,7 @@ colour_us <- "#e8931c"
 sea_colour <- "#ced6d9"
 land_colour <- "#c7c4bb"
 border_colour <- "#e3e1dc"
-  
+
 world <- gisco_get_countries()
 
 
@@ -101,13 +101,11 @@ subtitle_text <-str_c("Approximate area of fires in <br> <span style = 'color:",
                  " million hectares)</span> <br> and <span style = 'color:",
                  colour_us,";'>Western USA (",
                  round(conv_unit(drop_units(area_burnt_us),"m2","hectare")/10^6,2),
-                 " million hectares)</span>.")                        
+                 " million hectares)</span>.")
 
-caption_text <- "**Sources**: Eurostat,Wikipedia <br> <img src='https://upload.wikimedia.org/wikipedia/de/thumb/9/9f/Twitter_bird_logo_2012.svg/200px-Twitter_bird_logo_2012.svg.png'
-    height='9' />@carlosyanez"
-                  
+caption_text <- str_c("**Sources**: Eurostat,Wikipedia <br> ",add_twitter_text("@carlosyanez"))
 
- p1<-  ggplot()+
+p1<-ggplot()+
   with_shadow(geom_sf(data=world,aes(geometry=geometry),fill=land_colour,colour=border_colour),
               colour="azure1",
               x_offset = 0.01,
@@ -117,27 +115,13 @@ caption_text <- "**Sources**: Eurostat,Wikipedia <br> <img src='https://upload.w
     colour=colour_aus),colour=colour_aus,sigma=10)) +
   with_bloom(with_outer_glow(with_inner_glow(
     geom_sf(data=(us_fires),aes(geometry=geometry),fill=colour_us,colour=colour_us,alpha=0.5),
-    colour=colour_us),colour=colour_us,sigma=10)) +  
+    colour=colour_us),colour=colour_us,sigma=10)) +
   coord_sf(xlim = c(-10, 35), ylim = c(35, 71), expand = FALSE) +
-  custom_map_theme(legend_pos = "none") +
-  theme(panel.background = element_rect(fill = sea_colour,
-                                        colour = NA),
-        plot.background = element_rect(fill = sea_colour, 
-                                       colour = NA),
-        plot.title.position = "plot",
-        plot.title = element_markdown(size = 14,hjust=0),
-        plot.subtitle = element_markdown(size = 12,hjust=0),
-        plot.caption = element_markdown(size = 9,hjust=0,vjust=0),
-        plot.margin=grid::unit(c(0,0,0,0), "mm")) +
+  custom_map_theme_md(legend_pos = "none",background_colour=sea_colour,plot_margin=c(3,0,2,0)) +
   labs(title=title_text,
        subtitle=subtitle_text,
-       caption=caption_text) 
-  
+       caption=caption_text)
 
-# from https://www.pmassicotte.com/post/removing-borders-around-ggplot2-graphs/
+save_image(p1,"bushfires.png")
 
-ggsave("bushfires2020.pdf",  device = cairo_pdf,p1,dpi=600)
-knitr::plot_crop("bushfires2020.pdf")
-bitmap <- pdftools::pdf_render_page("bushfires2020.pdf", dpi = 600)
-png::writePNG(bitmap, "bushfires2020.png")
-file.remove("bushfires2020.pdf")
+
